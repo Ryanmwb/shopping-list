c2eecb577ff742cb8949049af8db6e1297279c30
Ryan <ryanmwb@gmail.com>
2019-02-07 19:12:12 -0500

    add spec folder

---
+++ spec/integration/user_spec.js
@@ -0,0 +1,11 @@
+/* 
+
+describe("User Integration:", () => {
+    describe("create user", () => {
+        it("user should not be null, and should have a name of 'alex'", (done) => {
+            expect(err, body, title, res.statusCode).toContain()//.toBeNull() .toBe() .not.toBeNull() 
+        })
+    })
+})
+
+*/
